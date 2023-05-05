8b8bd1c58a4f5ee4d381507be5b57700692a8cd4
Rafael Pereira <rafaelpersan@gmail.com>
2023-05-05 19:06:31 -0300

    primeira aula de DOOM

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+function v (){
+    document.querySelector('h1').style.backgroundColor = '#ff0000';
+}
+function a (){
+    document.querySelector('h1').style.backgroundColor = '#00008B';
+}
+function ve (){
+    document.querySelector('h1').style.backgroundColor = '#008000';
+}
+
+
+
+
+
+document.querySelector('#vermelho').addEventListener('click', v);
+
+document.querySelector('#azul').addEventListener('click', a);
+
+document.querySelector('#verde').addEventListener('click', ve);
